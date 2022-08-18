dd84713336d1063774103ab518bfeaa13efaa741
Angelique Abacajan <ma_utsey@yahoo.com>
2022-08-18 18:19:21 -0400

    Implemented additional variables

--- src/helloWorld.java
+++ src/helloWorld.java
@@ -3,7 +3,10 @@ public class helloWorld {
     public static void main(String[] args) {
         System.out.printIn("Hello, Angelique");
 
-        int myFirstNumber = 5;
+        int myFirstNumber = (10 + 5) + (2  * 10);
+        int mySecondNumber = 12;
+        int myThirdNumber = 6;
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
         System.out.printIn("myFirstNumber");
 
     }

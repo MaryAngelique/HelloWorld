59e222de4d506324b55bfbb53ad5feba8fab3531
Angelique Abacajan <ma_utsey@yahoo.com>
2022-08-18 17:51:15 -0400

    Initial commit

---
+++ src/helloWorld.java
@@ -0,0 +1,10 @@
+public class helloWorld {
+
+    public static void main(String[] args) {
+        System.out.printIn("Hello, Angelique");
+
+        int myFirstNumber = 5;
+        System.out.printIn("myFirstNumber");
+
+    }
+}
